0595a950daf1f63ed6a1e0f3d5106c874215c856
Vlad Girko <vld.girko@gmail.com>
2017-12-18 23:51:49 +0200

    fourth commit

--- src/Tester.java
+++ src/Tester.java
@@ -3,13 +3,6 @@ import java.util.Random;
 public class Tester {
 	
 	public static void main(String[] args) {
-		Random rnd = new Random();
-		System.out.println("Hellow world");
-		second scnd = new second("Cобачка бежала");
-		int x = rnd.nextInt(2);
-		if(x == 1) scnd.demon();
-		if(x == 0) System.out.println("Pedro was here today");
-		System.out.println("last string of this program");
 		System.out.println("checked");
 	}
 	
--- src/second.java
+++ src/second.java
@@ -1,11 +1,4 @@
 
 public class second {
-	String text;
-	public second(String text) {
-		this.text = text;
-	}
-	
-	public void demon() {
-		System.out.println(text);
-	}
+
 }
